5a94e03af597eb1be40615e0db30120a8970bdf2
Soham Karandikar <sohamk10@gmail.com>
2024-07-27 19:12:13 +0530

    feat: landing page
    1. Site title and header title updates for every page individually.
    2. Added self introduction on landing page
    3. Changed about section to projects.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_DESCRIPTION = `Soham Karandikar is a software engineer who loves to build things. He is currently a third-year B. Tech. student at the Vishwakarma Institute of Technology, Pune, India. He is passionate about web development, machine learning, and open-source software.`;
