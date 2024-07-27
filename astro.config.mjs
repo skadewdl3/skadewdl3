74c3c549da8f92b5ae335c685c5b3a0fe16f669d
Soham Karandikar <sohamk10@gmail.com>
2024-07-27 21:55:23 +0530

    feat: index page
    1. Completed index page (for now)
    2. Linked blog, projects and index together
    3. Modified navbar to not show current page in the right side links
    4. Added contact links in footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,19 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind()],
-    markdown: {
-        shikiConfig: {
-          // Choose from Shiki's built-in themes (or add your own)
-          // https://shiki.style/themes
-          // Alternatively, provide multiple themes
-          // See note below for using dual light/dark themes
-          themes: {
-            light: 'poimandres',
-            dark: 'catppuccin-latte',
-          },
-        },
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://shiki.style/themes
+      // Alternatively, provide multiple themes
+      // See note below for using dual light/dark themes
+      themes: {
+        light: 'poimandres',
+        dark: 'catppuccin-latte',
       },
-    
+    },
+  },
+
 })
