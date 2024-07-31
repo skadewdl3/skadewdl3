1b972df527dcba3e4d133c736919806fc707272e
Soham Karandikar <sohamk10@gmail.com>
2024-07-31 11:49:48 +0530

    Fixed blog paddig on mobile

--- src/components/blog/1-marching-squares/Sinx.vue
+++ src/components/blog/1-marching-squares/Sinx.vue
@@ -1,24 +1,36 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import initP5 from '../../../utils/initP5'
 
 
+let points = [];
+
 function setup(width, height) {
-  console.log(width, height)
   this.createCanvas(width, height);
+  for (let i = 0; i < width; i += 20) {
+    let x = i;
+    let y = (height / 2) + this.sin(x) * 20
+    points.push(this.createVector(x, y));
+  }
+  console.log(points)
 }
 
 function draw() {
-  this.background(0)
+  this.background(0, 255, 0, 0)
+  this.stroke(255, 0, 0)
+  this.strokeWeight(3)
+  for (let i = 0; i < points.length - 1; i++) {
+    this.point(points[i].x, points[i].y)
+  }
 }
 
+const p5Root = ref(null)
+
 onMounted(() => {
-  let root = document.querySelector('.p5-sketch')
-  let sketch = initP5(root, 3 / 16, setup, draw)
+  let sketch = initP5(p5Root.value, 3 / 16, setup, draw)
 })
 </script>
 
 <template>
-  <div class="p5-sketch w-full"></div>
-  This is from the sinx file
+  <div class="p5-sketch w-full" ref="p5Root"></div>
 </template>
