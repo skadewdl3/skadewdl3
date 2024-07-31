6b1e441aae6f1a47d0801b4448d6874aaa619dcb
Soham Karandikar <sohamk10@gmail.com>
2024-07-31 17:38:03 +0530

    got started with marching squares blog post (finally!!)

--- src/components/blog/1-marching-squares/Sinx.vue
+++ src/components/blog/1-marching-squares/Sinx.vue
@@ -1,49 +1,47 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import initP5 from '../../../utils/initP5'
 
 const p5Root = ref(null)
+const resolution = ref(51)
+const frequency = ref(1)
 
 onMounted(() => {
   let sketch = initP5(p5Root.value, 3 / 16, setup, draw)
 })
 
+watch([resolution, frequency], updatePoints)
+
 let points = []
-let resolution
 let origin
-let frequency
+let p
 
 function updatePoints() {
   points = []
-  let n = resolution.value()
+  let n = resolution.value
   for (let i = 0; i < n + 1; i += 1) {
-    let x = this.width * (i / resolution.value());
-    let y = this.height / 2 * Math.sin(2 * frequency.value() * Math.PI * (i / n));
-    points.push(this.createVector(x, y))
+    let x = p.width * (i / resolution.value);
+    let y = ((p.height / 2) - 5) * Math.sin(2 * frequency.value * Math.PI * (i / n));
+    points.push(p.createVector(x, y))
   }
 }
 
 function setup(width, height) {
-  this.createCanvas(width, height);
-  resolution = this.createSlider(2, 100, 5, 1)
-  resolution.input(updatePoints.bind(this))
+  p = this
+  p.createCanvas(width, height);
 
-  frequency = this.createSlider(0.5, 4, 1, 0.1)
-  frequency.input(updatePoints.bind(this))
-
-
-  origin = this.createVector(0, height / 2);
-  updatePoints.bind(this)()
+  origin = p.createVector(0, height / 2);
+  updatePoints()
 }
 
 function draw() {
-  this.background(0)
-  this.stroke(255, 0, 0)
-  this.strokeWeight(5)
+  p.background(0)
+  p.stroke(255, 0, 0)
+  p.strokeWeight(2)
 
-  this.translate(origin);
-  for (let point of points) {
-    this.point(point.x, point.y)
+  p.translate(origin);
+  for (let i = 0; i < points.length - 1; i++) {
+    p.line(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y)
   }
 }
 
@@ -51,4 +49,16 @@ function draw() {
 
 <template>
   <div class="p5-sketch w-full" ref="p5Root"></div>
+  <br />
+  <div class="grid grid-cols-2 place-items-center">
+    <div class="sinx-control text-center">
+      <p>Resolution</p>
+      <input type="range" min="3" max="99" step="1" v-model="resolution" />
+    </div>
+    <div class="sinx-control text-center">
+      <p>Frequency</p>
+      <input type="range" min="0.5" max="4" step="0.1" v-model="frequency" />
+    </div>
+  </div>
+  <br />
 </template>
